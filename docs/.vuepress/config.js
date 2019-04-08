96f7d9f52916680b34503e272f0a54e45f895f7e
Even <evendesign@gmail.com>
2019-04-08 18:01:45 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'zh-TW', // 将会被设置为 <html> 的 lang 属性
+      title: '美勞哥的美勞作業',
+      description: '美勞哥的各種筆記',
+    }
+  },
+  markdown: {
+    config: md => {
+      md.use(require('markdown-it-tetris')).use(require('markdown-it-puyo'))
+    }
+  },
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "/app.css"
+      }
+    ]
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,32 @@
+# Tetris code
+
+::: tip 秘訣！
+這是攻略喔！！
+:::
+
+## DT 砲
+
+tetris {
+
+
+xxLL
+xx L     I
+JJ LT  OOI
+J  TTTiOOI
+J   zjilsI
+oo zzjilss
+oo zjjills
+ggg gggggg
+G GGGGGGGG
+}
+
+## GTR
+
+puyo {
+
+gp
+by
+gr
+ggr
+rr
+}
